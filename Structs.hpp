2e4c73a00bc2c81c250ce9f65a6b445993a934be
Leandro Bandeira <leoviolinist22@gmail.com>
2025-08-17 12:16:06 -0300

    Adicionando ro para alguns casos

--- Structs.hpp
+++ Structs.hpp
@@ -4,6 +4,17 @@
 #include <vector>
 using namespace std;
 
+/* Struct referente ao contexto */
+struct Context{
+  int counter; /* Contador total de elementos do contexto */
+  int counter_ro; /* Contador do ro */
+  
+  Context(){
+    counter = 0;
+    counter_ro = 0;
+  }
+};
+
 struct TNode{
   vector<bool>alphabet;
   double probValue;   
@@ -13,7 +24,9 @@ struct TNode{
   TNode* vine;
   TNode* rigthPointer;
   TNode* downPointer;
-
+  TNode* lastCreate; /* Ultimo no criado do pai */
+  Context* context;
+  
   TNode() {
         symbol = 0;
         counter = 0;
@@ -22,6 +35,8 @@ struct TNode{
         vine = nullptr;
         rigthPointer = nullptr;
         downPointer = nullptr;
+        context = nullptr;
+        lastCreate = nullptr;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@
 
 using namespace std;
 
+
+TNode* createChild(TNode* father, unsigned char symbol){
+    TNode* child = new TNode();
+    if(father->isroot)child->vine = father;
+    
+    child->counter += 1;
+    child->symbol = symbol;
+    father->lastCreate = child;
+    return child;
+}
+
+
 int main(){
     TNode root;
     root.isroot = true;
@@ -33,51 +45,93 @@ int main(){
     int count = 0;
     for(unsigned char c: msg){
         double currentProb = 0.0;
+        double currentProb_ro = 0.0;
         cout << "Lendo: " << c << "\n";
-        TNode* father = base;
-        while(father != nullptr){     
+        TNode* cBase = base;
+        TNode* childCreated = nullptr; /* Ultimo vine acessado */
+        
+        while(cBase != nullptr){     
             /* Verificamos se base esta apontado para o root */
-            if(father->isroot){
+            if(cBase->isroot){
                 int idx = static_cast<int>(c);
                 /* Verificamos se o alfabeto esta na raiz, caso sim
                    Vamos codificar de acordo com equiprobabilidade e remover o alfabeto
                 */
-                if(father->alphabet[idx]){
-                    father->alphabet[c] = false;
-                    currentProb = father->probValue;
+                if(cBase->alphabet[idx]){
+                    cBase->alphabet[c] = false;
+                    currentProb = cBase->probValue;
                     cout << "O caracter (" << c << ") foi codificado com probabilidade: " << currentProb << "\n";
+                    
                     /* Precisamos atualizar a probabilidade do root */
-                    father->counter--;
-                    father->probValue =  1.0 / father->counter;
+                    cBase->counter--;
+                    cBase->probValue =  1.0 / cBase->counter;
+                
+                    /* Criando filho da raiz */
+                
+                    TNode* child = createChild(&root, c);
+                
+                    if (count == 0){ 
+                        base = child;
+                        count += 1;
+                        root.downPointer = child;
+                        root.context = new Context();
+                        root.context->counter_ro += 1;
+                        root.context->counter = root.context->counter_ro + 1;
+                    }
+                    else{
+                        currentProb_ro = (double)root.context->counter_ro / root.context->counter;
+                        cout << "Codificacao do ro: " << currentProb_ro << "\n";
+                        root.context->counter_ro += 1;
+                        root.context->counter += 2;
+                        TNode* lastCreate = root.lastCreate;
+                        lastCreate->rigthPointer = child;
+                        root.lastCreate = child;
+                    }
+                
+                    if(childCreated)childCreated->vine = child;
                 }
-                /* Criando filho da raiz */
-                TNode* child = new TNode();
-                child->vine = &root;
-                child->symbol = c;
-                child->counter += 1;
-                if (count == 0) base = child; /* Atualiza o base */
-                count += 1;
+                else{
+                    /* Caso ele nao exista na raiz, vamos percorrer os filhos da raiz */
+                    TNode* cNode = root.downPointer;
+
+                    while(cNode){
+                        if(cNode->symbol == c){
+                            currentProb = (double)cNode->counter / root.context->counter;
+                            cout << "Codificando (" << c << ") em k == 0 com probabilidade: " << currentProb << "\n";
+                            cNode->counter++;
+                            break;
+                        }
+                        cNode = cNode->rigthPointer;
+                    }    
+                }
                 break;
             
             }else{          
-                char currentSymbol = father->symbol;
+                char currentSymbol = cBase->symbol;
+                cout << "Acessando node com simbolo: " << currentSymbol << "\n";
                 if (currentSymbol == c){
-                    father->counter += 1;
+                    cBase->counter += 1;
                     break;
                 }
 
-                /* Verifica se possui filhos */
-                if(!father->downPointer){
+                /* Verifica se possui filhos, ou seja, se o contexto existe */
+                if(!cBase->downPointer){
                     cout << "Criou o filho: " << c << "\n";
-                    TNode* child = new TNode();
-                    child->symbol = c;
-                    child->vine = father;
-                    child->counter = 1;
+                    TNode* child = createChild(cBase, c);
+                    childCreated = child;            
+                    cBase->downPointer = child;
+
+                    /* Adiciona o contexto criado */
+                    Context* context = new Context();
+
+                    context->counter_ro += 1;
+                    context->counter = context->counter_ro + 1;
+                    child->context = context;
+
+                    /* Atualiza o base */
                     base = child;
-                    father->downPointer = child;
-                    
                 }
-                father = father->vine;
+                cBase = cBase->vine;
             }
         
         }     
